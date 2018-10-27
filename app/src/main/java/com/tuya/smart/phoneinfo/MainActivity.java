fbacff088d24961c64b99d69a3cae309d5c4bd42
xushun <xushun@tuya.com>
2018-10-27 17:20:52 +0800

    Initial commit

---
+++ app/src/main/java/com/tuya/smart/phoneinfo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tuya.smart.phoneinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textview);
+
+
+        PhoneInfoUtils phoneInfoUtils = new PhoneInfoUtils(this);
+        textView.setText(phoneInfoUtils.getPhoneInfo());
+
+    }
+}
